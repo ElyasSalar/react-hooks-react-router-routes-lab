c44d75849e447060d3fda39dc0bc9f3febbd1b11
Elyas <elyassalar8@gmail.com>
2021-08-12 17:28:20 +0300

    36th assignment solution

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className='navbar'>
+      <ul>
+        <li><NavLink exact to='/' activeClassName='active'>Home</NavLink></li>
+        <li><NavLink exact to='/movies' activeClassName='active'>Movies</NavLink></li>
+        <li><NavLink exact to='/directors' activeClassName='active'>Directors</NavLink></li>
+        <li><NavLink exact to='/actors' activeClassName='active'>Actors</NavLink></li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
